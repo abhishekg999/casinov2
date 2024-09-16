bc2ac492b740b4945c52a673e72936d6d48fa486
Abhishek Govindarasu <abhishekgovindarasu@gmail.com>
2024-09-16 00:56:56 -0700

    oops

--- server/mines_handler.py
+++ server/mines_handler.py
@@ -8,24 +8,3 @@ class MinesParams(BaseModel):
     secret: str
     mine_count: int
     
-
-async def mines_ws(websocket: WebSocket):
-    await websocket.accept()
-    try:
-        data = await websocket.receive_json()
-        params = MinesParams(**data)
-        game = MinesGame(**params.model_dump())
-        
-        while True:
-            data = await websocket.receive_json()
-            if "action" not in data:
-                continue
-            if data["action"] == "claim" and game.state == "playing":
-                await websocket.send_json(
-                    {
-                        "type": "claim",
-                        "value": round(game.bet * game.current_multiplier, 2),
-                    }
-                )
-                game.claimed = True
-                return
